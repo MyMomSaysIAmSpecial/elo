b7934303e23c869316db4244369262ba653e377d
Roman Taranenko <roman@aruodas.lt>
2016-09-07 12:56:20 +0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple php elo calculator based on [Arpad Elo elo calculating method](https://en.wikipedia.org/wiki/Elo_rating_system)
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+class EloCalculator
+{
+    // The maximum possible adjustment per game, called the K-factor,
+    // was set at K = 16 for masters and K = 32 for weaker players.
+    private $cooficient = 16;
+
+    // Player game result points for win, lose and draw
+    private $points = [
+        1,
+        0,
+        0.5
+    ];
+
+    /**
+     * @param int $player first player elo
+     * @param int $rival second player elo
+     * @param int $result first player game result
+     *
+     * @return float
+     */
+    public function calculate($player, $rival, $result)
+    {
+        $expectedValue = 1 / (1 + 10 ** (($rival - $player) / 400));
+        return $player + $this->cooficient * ($this->points[$result] - $expectedValue);
+    }
+}
+
+$eloCalculator = new EloCalculator();
+$r = $eloCalculator->calculate(1400, 1400, 0);
